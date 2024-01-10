b93075ec3760df3af3dcb44e8db219b0a54071f2
AdriaMartinez-231 <am76635@gmail.com>
2024-01-10 17:11:49 +0100

    Maps en un diccionario
    Se ponen todos los mapas del juego en un solo diccionario

--- M03/Zelda/Project/maps.py
+++ M03/Zelda/Project/maps.py
@@ -2,12 +2,76 @@
 import os
 from funciones import player
 from funciones import * 
-x = "X"
 
+x = "X"
 
 last_player = list(player.keys())[-1]
 sanctuary_value0 = player[last_player]["sanctuaries"]["S0"]["name"]
 sanctuary_value1 = player[last_player]["sanctuaries"]["S1"]["name"]
+sanctuary_value2 = player[last_player]["sanctuaries"]["S2"]["name"]
+sanctuary_value3 = player[last_player]["sanctuaries"]["S3"]["name"]
+sanctuary_value4 = player[last_player]["sanctuaries"]["S4"]["name"]
+sanctuary_value5 = player[last_player]["sanctuaries"]["S5"]["name"]
+sanctuary_value6 = player[last_player]["sanctuaries"]["S6"]["name"]
+
+maps = {
+    "hyrule" : [
+        f"\n* Hyrule  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *",
+        f"\n*                                    ~~~~~~~~~~~~~~~~~~OOO*                   *",
+        f"\n*                                    ~~~~~~~~~~~~~OO~OOOO~*                   *",
+        f"\n*                  C                      ~~~~~~    ~~~~~~*                   *",
+        f"\n*    T                                                 ~~~*                   *",
+        f"\n*                                 E9                      *                   *",
+        f"\n*                                          {sanctuary_value0}            *                   *",
+        f"\n*                                                         *                   *",
+        f"\n*           {x}                                   T         *                   *",  # La X es el personaje (cambiar posiblemente)
+        f"\n* OO    OOOOO       E1        {sanctuary_value1}             T M   F     *                   *",
+        f"\n*OOOOOOOOOOO                                              *                   *",
+        f"\n* Exit, Attack, Go, Equip, Unequip, Eat, Cook, Fish, Open * * * * * * * * * * *"
+    ],
+    "death mountain" : [
+        f"\n* Death Mountain  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *",
+        f"\n* O                OOOO                                   *                   *",
+        f"\n* O                 OOOO      F                           *                   *",
+        f"\n* ~~  {sanctuary_value2}            OOOO                          E2     *                   *",
+        f"\n* ~~~        E2      OOOO      OOOO                       *                   *",
+        f"\n* O~~~~~~~~            OOOO  OO    OOOOOOOO               *                   *",
+        f"\n* ~~~~~~~~~             OOOOOO           OOOO             *                   *",
+        f"\n*    ~~~           T        OOOO           OO             *                   *",
+        f"\n*                 T          OO     M                     *                   *",
+        f"\n* {x}   C           T          OO                  {sanctuary_value3}      *                   *",
+        f"\n*                                                         *                   *",
+        f"\n* Exit, Attack, Go, Equip, Unequip, Eat, Cook, Fish, Open * * * * * * * * * * *"  
+    ],
+    "gerudo" : [
+        f"\n* Gerudo  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *",
+        f"\n*OOOOOOOOOOOOOOOO                                   M     *                   *",
+        f"\n*  OOOOO  OOOOO              TTT                          *                   *",
+        f"\n*                              TT             {sanctuary_value4}         O*                   *",
+        f"\n*  E1          C                                        OO*                   *",
+        f"\n*                                                       OO*                   *",
+        f"\n*             AAAAAA                  E2                  *                   *",
+        f"\n*             AAAAAAAA                                    *                   *",
+        f"\n*    T       AAAAAAA                 OOOOO      F       ~~*                   *",
+        f"\n* {x}     M      AAA        OOOOO    OOOOO              ~~~~*                   *",
+        f"\n* Exit, Attack, Go, Equip, Unequip, Eat, Cook, Fish, Open * * * * * * * * * * *"
+    ],
+    "necluda" : [
+        f"\n* Necluda * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *",
+        f"\n*                     M                                   *                   *",
+        f"\n* !       E1                         TT            M      *                   *",
+        f"\n*OO                C               TT                ~~~~~*                   *",
+        f"\n*OOOOO                                           ~~~~~~~~~*                   *",
+        f"\n*OOOO                                              ~~~~~~~*                   *",
+        f"\n*              T                      E2           {sanctuary_value5}~~~~~*                   *",
+        f"\n*     F       T9                                ~~~~~~~~~~*                   *",
+        f"\n*~~            T6                                  ~~~~~~~*                   *",
+        f"\n*~~~~~~~~              M         {sanctuary_value6}          ~~~~~~~~~~~~~*                   *",
+        f"\n*~~~~~~~~~~~~                           ~~~~~~~~~~~~~~~~~~*                   *",
+        f"\n* Exit, Attack, Go, Equip, Unequip, Eat, Cook, Fish, Open * * * * * * * * * * *"
+    ]
+}
+
 hyrule = [
     f"\n* Hyrule  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *",
     f"\n*                                    ~~~~~~~~~~~~~~~~~~OOO*                   *",
@@ -76,13 +140,3 @@ while lp:
         continue
 
     move_player()
-
-
-# def combine_maps(hyrule, inventoryM):
-#     for i in range(max(len(hyrule), len(inventoryM))):
-#         if i < len(hyrule):
-#             hyrule_line = hyrule[i] if i < len(hyrule) else ""
-#             inventory_line = inventoryM[i] if i < len(inventoryM) else ""
-#             print(hyrule_line + " " * (40 - len(hyrule_line)) + inventory_line)
-#         else:
-#             print(" " * 40 + inventoryM[i])
