0a593d18590b2cf9454e9e10bd49594fc57d2873
elaminmohae <elaminmohae@gmail.com>
2024-01-15 18:13:41 +0100

    Query Trials

--- M03/Zelda/Project/consultas.py
+++ M03/Zelda/Project/consultas.py
@@ -35,7 +35,8 @@ def generate_insert_weapon_query(game_id, weapon_name, equipped=False, lives_rem
                       VALUES ({}, '{}', {}, {})""".format(game_id, weapon_name, equipped, lives_remaining)
 
     return insert_query
-s
+
 # Ejemploo
 weapon_query = generate_insert_weapon_query(game_id=1, weapon_name="Espada", equipped=True, lives_remaining=3)
 print(weapon_query)
+weapon_query()
