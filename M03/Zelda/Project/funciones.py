cb11247fb3035d5c29ced071048e537c5b71bf54
AdriaMartinez-231 <am76635@gmail.com>
2024-01-15 15:37:44 +0100

    Update funciones.py

--- M03/Zelda/Project/funciones.py
+++ M03/Zelda/Project/funciones.py
@@ -6,10 +6,21 @@ from consultas import *
 import random
 import os
 import pymysql
+import mysql.connector
 
 conn = pymysql.connect(host="localhost", user="root", password="root", db="Zelda")
 cur = conn.cursor()
 
+
+############################### QUERYS ##############################
+
+def db_query(qry):
+    cnx = mysql.connector.connect(user='root', password='pwd', host='127.0.0.1', database='world')
+    cur = cnx.cursor()
+    cur.execute("SELECT * FROM city")
+    rst = cur.fetchall()
+    cnx.close()
+    return rst
 
 
 ###############################  OPCIONES MENU INICIO ##############################
